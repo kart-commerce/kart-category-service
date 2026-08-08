e2c3eb012d18d00b3323b9b3494e27781ea99960
kakon-selise <kakon.mehedi@selisegroup.com>
2026-08-08 18:07:15 +0600

    add docker cashing

--- src/Infrastructure/DependencyInjection.cs
+++ src/Infrastructure/DependencyInjection.cs
@@ -41,7 +41,7 @@ public static class DependencyInjection
         services.AddKartRabbitMqConnectionFactory(sp =>
         {
             var options = sp.GetRequiredService<IOptions<RabbitMqOptions>>().Value;
-            return new RabbitMqConnectionSettings(options.HostName);
+            return new RabbitMqConnectionSettings(options.HostName, UserName: options.UserName, Password: options.Password);
         });
         services.AddKartRabbitMqTopologyStartup();
         services.AddHostedService<OutboxRelayHostedService>();
